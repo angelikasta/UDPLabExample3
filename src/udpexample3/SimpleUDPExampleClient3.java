2e6df98f8b5e6131822a32dc4ee42243db20da65
Matthew <matbulat@gmail.com>
2018-02-26 11:04:09 +0000

    Init Commit

---
+++ src/udpexample3/SimpleUDPExampleClient3.java
@@ -0,0 +1,33 @@
+package udpexample3;
+
+import java.io.*;
+import java.net.*;
+
+public class SimpleUDPExampleClient3 {
+
+    public static void main(String[] args) throws Exception {
+        try (DatagramSocket clientSocket = new DatagramSocket()) {
+
+            while (true) {
+                byte[] sendData = new byte[1024];
+                byte[] receiveData = new byte[1024];
+
+                BufferedReader inFromUser
+                        = new BufferedReader(new InputStreamReader(System.in));
+                InetAddress IPAddress = InetAddress.getByName("localhost");
+
+                String sentence = inFromUser.readLine();
+                sendData = sentence.getBytes();
+
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+                clientSocket.send(sendPacket);
+
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                clientSocket.receive(receivePacket);
+
+                String modifiedSentence = new String(receivePacket.getData());
+                System.out.println("FROM SERVER:" + modifiedSentence);
+            }
+        }
+    }
+}
